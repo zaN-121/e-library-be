e59af373e313f7a61100319e141913de3c417f99
Ryan Hasbie <105176413+ryanhasbie@users.noreply.github.com>
2023-05-14 13:18:11 +0700

    create skleton e-library fe

---
+++ src/test/java/com/elibrary/group4/Group4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elibrary.group4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
