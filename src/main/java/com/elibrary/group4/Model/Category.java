bdb3bb16769f9580481a33a7ce646200cfd08bf1
Muhamad Yuda Nurjaya <muhammadyudanurjaya25@gmail.com>
2023-05-15 10:17:42 +0700

    [Feature]Create Model From Erd

---
+++ src/main/java/com/elibrary/group4/Model/Category.java
@@ -0,0 +1,19 @@
+package com.elibrary.group4.Model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "category")
+@Getter @Setter
+public class Category {
+    @Id
+    @Generated(value = "UUID")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "category_id")
+    private String categoryId;
+    @Column(name = "name",nullable = false)
+    private String name;
+}
---
+++ src/main/java/com/elibrary/group4/Model/User.java
@@ -0,0 +1,34 @@
+package com.elibrary.group4.Model;
+
+import com.elibrary.group4.Utils.Constants.Role;
+import jakarta.annotation.Generated;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_user")
+@Getter @Setter
+public class User {
+    @Id
+    @Generated(value = "UUID")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private String userId;
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+    @Column(name = "user_name",nullable = false)
+    private String userName;
+    @Column(name = "email", nullable = false)
+    @Email
+    private String email;
+    @Column(name = "phone", nullable = false)
+    private String phone;
+    @Column(name = "password",nullable = false)
+    private String password;
+    @Column(name = "Role",nullable = false)
+    private Role role;
+}
