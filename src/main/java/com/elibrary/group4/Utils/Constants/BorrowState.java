9b3c0ae8f6d55bbc6707cd8510869c43af75a246
Hafiz <hafiz.salam2k@gmail.com>
2023-05-16 18:23:19 +0700

    scheduler

--- src/main/java/com/elibrary/group4/Utils/Constants/BorrowState.java
+++ src/main/java/com/elibrary/group4/Utils/Constants/BorrowState.java
@@ -1,5 +1,5 @@
 package com.elibrary.group4.Utils.Constants;
 
 public enum BorrowState {
-    APPROVED, DECLINE
+    APPROVED, DECLINE, CANBETAKE, LATE, RETURN
 }
--- src/main/java/com/elibrary/group4/repository/BookRepository.java
+++ src/main/java/com/elibrary/group4/repository/BookRepository.java
@@ -2,9 +2,11 @@ package com.elibrary.group4.repository;
 
 import com.elibrary.group4.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,4 +17,15 @@ public interface BookRepository  extends JpaRepository<Book, String> {
     List<Book> findByPublisherContains(String publisher);
     @Query("SELECT b from Book b WHERE b.category.name =?1")
     List<Book> findByCategory(String category);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Book e SET e.stock = e.stock+1 WHERE id =?1")
+    public void stockIncrease(String id);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Book e SET e.stock = e.stock-1 WHERE id =?1")
+    public void stockDecrease(String id);
+
 }
